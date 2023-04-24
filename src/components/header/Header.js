62d6f27d8881411d72b11d7b7d2d511f96389452
DiegoOrtegaElRincon <diegoalejandroortegamanzano@alumno.ieselrincon.es>
2023-04-24 20:02:23 +0100

    first commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <ul>
+      <li><a href="/">HOME</a></li>
+      <li><a href="/review">REVIEW</a></li>
+      <li><a href="/forum">FORUM</a></li>
+      <li><a href="/contact">CONTACT</a></li>
+    </ul>
+  );
+}
+
+export default Header;
