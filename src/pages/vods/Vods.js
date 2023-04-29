994514d01615cbe51b8d9fad9f932ab3d91af42b
DiegoOrtegaElRincon <118464823+DiegoOrtegaElRincon@users.noreply.github.com>
2023-04-29 02:48:15 +0200

    Done VODs, falta contacto y review

--- src/pages/vods/Vods.js
+++ src/pages/vods/Vods.js
@@ -1,13 +1,13 @@
-import CoachingList from "../../components/coachinglist/CoachingList";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
+import VodReviewList from "../../components/vodreviewlist/VodReviewList";
 
 function Vods() {
   return (
     <>
       <Header />
       
-      <CoachingList/>
+      <VodReviewList/>
       <Footer />
     </>
   );
